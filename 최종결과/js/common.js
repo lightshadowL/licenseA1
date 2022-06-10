6f760406eb87f1cdac17896189ede169767cee0a
jong won <lsljw94@daum.net>
2022-06-10 12:10:38 +0900

    슬라이드

--- 최종결과/js/common.js
+++ 최종결과/js/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  
+  var count=0;
+  setInterval(function(){
+    if(count==3){
+      count=0;
+    }
+    $(".sliders").stop().animate({
+      left:-1200*count
+    })
+    count++;
+  },3000);
 });
